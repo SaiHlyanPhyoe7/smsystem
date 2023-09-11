282ff88664f558024185aa7c50f401e16978bd1e
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-11 19:46:13 +0630

    feat:add students entity

---
+++ src/main/java/net/sai/smsystem/entity/Student.java
@@ -0,0 +1,32 @@
+package net.sai.smsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "age")
+    private int age;
+}
