96ee6180d9817e47f06cde6b64f9a621ea7e4f41
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-11 19:54:04 +0630

    feat:add studentDto

---
+++ src/main/java/net/sai/smsystem/dto/StudentDto.java
@@ -0,0 +1,18 @@
+package net.sai.smsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+}
