a0207d03764bd34cb53336eff27e0330ff0ec5ec
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-11 19:30:55 +0630

    Initial Commit

---
+++ src/test/java/net/sai/smsystem/SchoolManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sai.smsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
