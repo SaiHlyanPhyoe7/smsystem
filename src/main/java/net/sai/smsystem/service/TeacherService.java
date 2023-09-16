369ef41db283a9f4df5a9a87adc306befd6ef15f
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-16 18:29:43 +0630

    feat:add service layer for teacher

--- src/main/java/net/sai/smsystem/service/TeacherService.java
+++ src/main/java/net/sai/smsystem/service/TeacherService.java
@@ -1,7 +1,17 @@
 package net.sai.smsystem.service;
 
+import net.sai.smsystem.dto.TeacherDto;
+
+import java.util.List;
+
 public interface TeacherService {
     TeacherDto createTeacher(TeacherDto teacherDto);
 
-    TeacherDto
+    TeacherDto getTeacherById(Long teacherId);
+
+    List<TeacherDto> getAllTeachers();
+
+    TeacherDto updateTeacher(Long teacherId, TeacherDto teacherDto);
+
+    void deleteTeacher(Long teacherId);
 }
