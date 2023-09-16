13a8a5bb815f10b626f96ba1b13c76b9ad0a6a71
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-16 18:38:14 +0630

    feat:add teacher service impl

---
+++ src/main/java/net/sai/smsystem/service/impl/TeacherServiceImpl.java
@@ -0,0 +1,33 @@
+package net.sai.smsystem.service.impl;
+
+import net.sai.smsystem.dto.TeacherDto;
+import net.sai.smsystem.service.TeacherService;
+
+import java.util.List;
+
+public class TeacherServiceImpl implements TeacherService {
+    @Override
+    public TeacherDto createTeacher(TeacherDto teacherDto) {
+        return null;
+    }
+
+    @Override
+    public TeacherDto getTeacherById(Long teacherId) {
+        return null;
+    }
+
+    @Override
+    public List<TeacherDto> getAllTeachers() {
+        return null;
+    }
+
+    @Override
+    public TeacherDto updateTeacher(Long teacherId, TeacherDto teacherDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteTeacher(Long teacherId) {
+
+    }
+}
