ca52e4b60d3f92befcc31ad34110ebc4fbb481d3
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-16 18:27:14 +0630

    feat:add entity for teacher

---
+++ src/main/java/net/sai/smsystem/entity/Teacher.java
@@ -0,0 +1,38 @@
+package net.sai.smsystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "teachers")
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email_id")
+    private String email;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "age")
+    private int age;
+
+    @Column(name = "experience_year")
+    private int yearOfExperience;
+}
