50262d1a11f6ac261c014e8d393db62b89b0186f
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-22 21:51:18 +0630

    feat:create Classroom Contoller

--- src/main/java/net/sai/smsystem/controller/ClassroomController.java
+++ src/main/java/net/sai/smsystem/controller/ClassroomController.java
@@ -1,7 +1,12 @@
 package net.sai.smsystem.controller;
 
 import lombok.AllArgsConstructor;
+import net.sai.smsystem.dto.ClassroomDto;
 import net.sai.smsystem.service.ClassroomService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/classrooms")
 public class ClassroomController {
     private ClassroomService classroomService;
+
+//    Build Add Classroom REST API
+    @PostMapping
+    public ResponseEntity<ClassroomDto> createClassroom(@RequestBody ClassroomDto classroomDto){
+        ClassroomDto saveClassroom = classroomService.createClassroom(classroomDto);
+        return new ResponseEntity<>(saveClassroom, HttpStatus.CREATED);
+    }
 }
