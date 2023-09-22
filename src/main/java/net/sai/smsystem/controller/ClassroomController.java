c68341e644700ea02797d890800ec580ab80dc1d
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2023-09-22 21:53:01 +0630

    feat:Added get classroom by id controller

--- src/main/java/net/sai/smsystem/controller/ClassroomController.java
+++ src/main/java/net/sai/smsystem/controller/ClassroomController.java
@@ -5,10 +5,7 @@ import net.sai.smsystem.dto.ClassroomDto;
 import net.sai.smsystem.service.ClassroomService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -21,5 +18,12 @@ public class ClassroomController {
     public ResponseEntity<ClassroomDto> createClassroom(@RequestBody ClassroomDto classroomDto){
         ClassroomDto saveClassroom = classroomService.createClassroom(classroomDto);
         return new ResponseEntity<>(saveClassroom, HttpStatus.CREATED);
+    }
+
+//    Build Get Classroom by id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<ClassroomDto> getClassroomById(@PathVariable("id") Long classroomId){
+        ClassroomDto classroomDto = classroomService.getClassroomById(classroomId);
+        return ResponseEntity.ok(classroomDto);
     }
 }
